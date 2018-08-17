10f651dd58c4604a19b805854acb55d46da69f1d
Mavveryk <33306726+Mavveryk@users.noreply.github.com>
2018-08-17 22:11:55 +0800

    Initial commit

---
+++ app/src/main/java/com/example/sotnas/armada20/PostData.java
@@ -0,0 +1,65 @@
+package com.example.sotnas.armada20;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class PostData extends AsyncTask<String, Void, Void> {
+
+
+    @Override
+    protected Void doInBackground(String... strings) {
+
+
+        String x = "http://localhost/armada-api/public/api/locations";
+        String response = "";
+        String[] sending = strings;
+
+        try {
+            URL url = new URL(x);
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(15000);
+            conn.setConnectTimeout(15000);
+            conn.setRequestMethod("POST");
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+
+
+            OutputStream os = conn.getOutputStream();
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+            writer.write(String.valueOf(sending));
+
+            writer.flush();
+            writer.close();
+            os.close();
+            int responseCode = conn.getResponseCode();
+
+            if (responseCode == HttpsURLConnection.HTTP_OK) {
+                String line;
+                BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                while ((line = br.readLine()) != null) {
+                    response += line;
+                }
+            } else {
+                response = "";
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Log.i("Status: ", response);
+
+        return null;
+    }
+}
